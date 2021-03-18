36787e149960ce00d2cf8cf3b4e1f9fa65f5b8bc
MuhammadYasin725 <78532196+MuhammadYasin725@users.noreply.github.com>
2021-03-18 17:05:47 +0600

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ admin.site.site_title = "Welcome to CODEx!"
 admin.site.index_title = "Welcome To CODEx Admin"
 
 urlpatterns = [
-    path('home', views.home, name='blog_home'),
+    path('', views.home, name='blog_home'),
     path('about', views.about, name='blog_about'),
     path('courses', views.courses, name='blog_courses'),
     path('contact', views.contact, name='blog_contact')
